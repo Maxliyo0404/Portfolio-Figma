f237f6b33dda079d4766412bfcd920f955d0cb24
Mahliyo <gasandigital13@gmail.com>
2026-05-20 15:38:31 +0500

    header start logo  nav fnish

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./Hero.css";
 import React from 'react'
 
 function Hero() {
+    const {t, i18n} = useTranslation();
   return (
     <div>Hero</div>
   )
