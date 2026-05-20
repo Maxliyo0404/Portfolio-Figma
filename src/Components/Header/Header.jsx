84523f45eb25168794096999a33a85459009ca86
Mahliyo <gasandigital13@gmail.com>
2026-05-20 14:35:43 +0500

    Portfolio-Figma struktura locals

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="container">
+            <div className="header-container">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
