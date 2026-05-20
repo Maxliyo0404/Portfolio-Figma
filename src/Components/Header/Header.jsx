46635719eb7a0151f0b5ab87844fad6b74883d30
Mahliyo <gasandigital13@gmail.com>
2026-05-20 14:48:04 +0500

    Portfolio-Figma struktura locals language

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,12 +4,17 @@ import React from 'react'
 import i18next from "i18next";
 function Header() {
     const {t, i18n} = useTranslation();
-
+    const handleChangeLanguage = (event) =>{
+        i18n.changeLanguage(event.target.value)
+    }
   return (
     <div className="header">
         <div className="container">
             <div className="header-container">
-
+              <a className="logo" href="#"></a>
+              <ul className="header-list">
+                <li><a className="header-link" href="#"></a></li>
+              </ul>
             </div>
         </div>
     </div>
