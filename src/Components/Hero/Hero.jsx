4555eb452e0f6ce03feabf55a8c0c0d24308998e
Mahliyo <gasandigital13@gmail.com>
2026-05-20 17:05:11 +0500

    hero start wrapper

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
             <div className="container"></div>
             <div className="hero-wrapper">
                 <div className="hero-left">
-                    <h6 className="hero-helo">{t,("hero.hero-helo")}</h6>
-                    <h2 className="hero-name">{t,("hero.hero-name")}</h2> 
-                    <h4 className="hero-titles">{t,("hero.hero-titles")}</h4>
-                    <p className="hero-text">{t,("hero.hero-text")}</p>
+                    <h6 className="hero-helo">{t("hero.hero-helo")}</h6>
+                    <h2 className="hero-name">{t("hero.hero-name")}</h2> 
+                   <h4 className="hero-titles" dangerouslySetInnerHTML={{ __html: t('hero.hero-titles') }} />
+                    <p className="hero-text">{t("hero.hero-text")}</p>
                 </div>
             </div>
         </div>
