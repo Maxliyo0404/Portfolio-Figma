a61b66dfc785502d55feb702ac6c75d1078d3c02
Mahliyo <gasandigital13@gmail.com>
2026-05-20 17:54:16 +0500

    hero start wrapper css responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,28 +5,33 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import hero_1 from "./image/hero_1.svg";
 
 function Hero() {
     const {t, i18n} = useTranslation();
   return (
     <div>
         <div className="hero">
-            <div className="container"></div>
+            <div className="container">
             <div className="hero-wrapper">
                 <div className="hero-left">
                     <h6 className="hero-helo">{t("hero.hero-helo")}</h6>
                     <h2 className="hero-name">{t("hero.hero-name")}</h2> 
                    <h4 className="hero-titles" dangerouslySetInnerHTML={{ __html: t('hero.hero-titles') }} />
                     <p className="hero-text">{t("hero.hero-text")}</p>
-                    <div className="icons">
-                        <div className="icon"><FaFacebookF /></div>
-                        <div className="icon"><FaTwitter /></div>
-                        <div className="icon"><FaInstagram /></div>
-                        <div className="icon"><FaLinkedinIn /></div>
+                    <div className="hero-icons">
+                        <div className="hero-icon"><FaFacebookF /></div>
+                        <div className="hero-icon"><FaTwitter /></div>
+                        <div className="hero-icon"><FaInstagram /></div>
+                        <div className="hero-icon"><FaLinkedinIn /></div>
                     </div>
-                    <button className="hero-btn"></button>
+                    <button className="hero-btn">{t("hero.hero-btn")}</button>
+                </div>
+                <div className="hero-right">
+                    <img className="hero-rasm" src={hero_1} alt="" />
                 </div>
             </div>
+        </div>
         </div>
 
     </div>
