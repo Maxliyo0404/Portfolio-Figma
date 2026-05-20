48bbb1b15274f1ffc54da0e6dcd449f5a01631cb
Mahliyo <gasandigital13@gmail.com>
2026-05-20 14:41:58 +0500

    Portfolio-Figma struktura locals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import React from 'react'
-
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-    
+    <Header/>
     </>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,11 +2,12 @@ import "./Header.css";
 import React from 'react'
 
 function Header() {
+    const {t, i18n} = useT
   return (
     <div className="header">
         <div className="container">
             <div className="header-container">
-                
+
             </div>
         </div>
     </div>
