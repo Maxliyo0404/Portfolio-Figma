5132a0e447effc43dfd7446a809dbd4043ed9765
Mahliyo <gasandigital13@gmail.com>
2026-05-20 15:11:00 +0500

    header start

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,10 @@ import React from 'react'
 import i18next from "i18next";
 function Header() {
     const {t, i18n} = useTranslation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
     const handleChangeLanguage = (event) =>{
         i18n.changeLanguage(event.target.value)
     }
@@ -12,13 +16,30 @@ function Header() {
         <div className="container">
             <div className="header-container">
               <a className="logo" href="#">{t("header.logo")}</a>
-              <ul className="header-list">
-                <li><a className="header-link" href="#">{t("header.home")}</a></li>
-                <li><a className="header-link" href="#">{t("header.about")}</a></li>
-                <li><a className="header-link" href="#">{t("header.services")}</a></li>
-                <li><a className="header-link" href="#">{t("header.portfolio")}</a></li>
-                <li><a className="header-link" href="#">{t("header.contact")}</a></li>
-              </ul>
+             <nav className={`header-nav ${isMenuOpen ? "active" : ""}`}>
+                  <ul className="header-list">
+                    <li><a className="header-link" href="#">{t("header.home")}</a></li>
+                    <li><a className="header-link" href="#">{t("header.about")}</a></li>
+                    <li><a className="header-link">{t("header.service")}</a></li>
+                    <li><a className="header-link" href="#">{t("header.portfolio")}</a></li>
+                    <li><a className="header-link">{t("header.contact")}</a></li>
+                  </ul>
+        
+                  <div className="header-actions">
+                    <select className="select" onChange={handleChangeLanguage} value={i18n.language}>
+                      <option value="uz">Uz</option>
+                      <option value="en">Eng</option>
+                      <option value="ru"> Rus</option>
+                    </select>
+                    <button className="header-btn">{t("header.btn")}</button>
+                  </div>
+           </nav>
+      <div className={`burger-menu ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+             
             </div>
         </div>
     </div>
