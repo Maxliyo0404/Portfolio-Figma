5407ccbaa23a77bc726a18d561020af507a10952
Mahliyo <gasandigital13@gmail.com>
2026-05-21 16:24:33 +0500

    sectionThree start wrapper form

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./SectionThree.css";
 import React from 'react'
+import axios from "axios";
 
 function SectionThree() {
     const [t, i18n] = useTranslation();
@@ -16,13 +17,28 @@ function SectionThree() {
         const textarea = document.getElementById("textarea").value
         
          const messageContent = `Name : ${name}\n Email: ${email} \n Phone number: ${phone}\n Text: ${text} \n Textarea: ${textarea}`;
+          axios({
+               url:url,
+               method:"POST",
+               data :{
+                    chat_id:chat_id,
+                    text:messageContent,
+              }
+                 })
+                 .then(()=>{
+                   alert("Successfully sent!✅");
+                 }).catch((error)=>{
+                   console.log(error);
+    
+             })
+  
     }
   return (
     <div className="sectionThree">
         <div className="container">
             <div className="three-wrapper">
             <h2 className="three-title" dangerouslySetInnerHTML={{ __html: t('sectionThree.three-title') }} /> 
-            <form className="form">
+            <form className="form" onSubmit={sendMessage}>
             <div className="input-group">
               <input id="username" className="input" type="text" required placeholder={t("sectionThree.name-placeholder")} />
               <input id="email" className="input" type="email" required placeholder={t("sectionThree.email-placeholder")} />
@@ -33,7 +49,7 @@ function SectionThree() {
               <input id="text" className="input" type="text" required placeholder={t("sectionThree.subject-placeholder")} />
             </div>
                 <textarea id="textarea" className="textarea" required placeholder={t("sectionThree.message-placeholder")}></textarea>
-                <button className="three-btn">{t("sectionThree.btn-send")}</button>
+                <button type="submit" className="three-btn">{t("sectionThree.btn-send")}</button>
             </form>
             </div>
         </div>
