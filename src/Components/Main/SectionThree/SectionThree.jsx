60d80ea769b7c148e268a6d07fb8d1c1ca2cd55e
Mahliyo <gasandigital13@gmail.com>
2026-05-21 16:16:08 +0500

    sectionThree start wrapper

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -3,18 +3,33 @@ import "./SectionThree.css";
 import React from 'react'
 
 function SectionThree() {
-    const [t, i18n] = useTranslation
+    const [t, i18n] = useTranslation();
+    const sendMessage = (event) =>{
+        event.preventDefault();
+         const token = `8799654997:AAFXbpKmshRxD_WcqAShMxtcHeU-IMsZ2OE`
+        const chat_id =  647264939
+        const url = `https://api.telegram.org/bot${token}/sendMessage`;
+        const name = document.getElementById("username").value
+        const email = document.getElementById("email").value
+        const phone = document.getElementById("phone").value
+        const text = document.getElementById("text").value
+    }
   return (
     <div className="sectionThree">
         <div className="container">
             <div className="three-wrapper">
             <h2 className="three-title" dangerouslySetInnerHTML={{ __html: t('sectionThree.three-title') }} /> 
             <form className="form">
-                <input className="input" type="text"required placeholder={t("sectionThree.name-placeholder")} />
-                <input className="input" type="email"required placeholder={t("sectionThree.email-placeholder")} />
-                <input className="input" type="number"required placeholder={t("sectionThree.phone-placeholder")} />
-                <input className="input" type="text"required placeholder={t("sectionThree.subject-placeholder")} />
-                <textarea  className="textarea"></textarea>
+            <div className="input-group">
+              <input id="username" className="input" type="text" required placeholder={t("sectionThree.name-placeholder")} />
+              <input id="email" className="input" type="email" required placeholder={t("sectionThree.email-placeholder")} />
+            </div>
+        
+            <div className="input-group">
+              <input id="phone" className="input" type="number" required placeholder={t("sectionThree.phone-placeholder")} />
+              <input id="text" className="input" type="text" required placeholder={t("sectionThree.subject-placeholder")} />
+            </div>
+                <textarea className="textarea" required placeholder={t("sectionThree.message-placeholder")}></textarea>
                 <button className="three-btn">{t("sectionThree.btn-send")}</button>
             </form>
             </div>
@@ -25,4 +40,3 @@ function SectionThree() {
 
 export default SectionThree
 
-   
