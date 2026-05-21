25b3fa8dfb10a37c69e96fc72b55d55b203bf742
Mahliyo <gasandigital13@gmail.com>
2026-05-21 13:05:53 +0500

    sectionOne wrapper i18n css list

--- src/Components/Main/SectionOne/SectionOne.jsx
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -10,23 +10,23 @@ function SectionOne() {
     <div className="sectionOne">
       <div className="container">
         <div className="sectionOne-wrapper">
-          <h2 className="one-title" dangerouslySetInnerHTML={{ __html: t('sectionOne.one-title') }} />
+          <h2 className="one-titles" dangerouslySetInnerHTML={{ __html: t('sectionOne.one-title') }} />
           <ul className="one-list">
             <li className="one-item">
-              <div className="icon"><FaCode /></div>
-              <h3 className="">{t("sectionOne.one-web")}</h3>
+              <div className="one-icon"><FaCode /></div>
+              <h3 className="one-title">{t("sectionOne.one-web")}</h3>
               <p className="one-text">{t("sectionOne.one-text")}</p>
               <button className="one-btn">{t("sectionOne.one-btn")}</button>
             </li>
             <li className="one-item">
-              <div className="icon"><FaPaintBrush /></div>
-              <h3 className="">{t("sectionOne.one-graphic")}</h3>
+              <div className="one-icon"><FaPaintBrush /></div>
+              <h3 className="one-title">{t("sectionOne.one-graphic")}</h3>
               <p className="one-text">{t("sectionOne.one-text")}</p>
               <button className="one-btn">{t("sectionOne.one-btn")}</button>
             </li>
             <li className="one-item">
-              <div className="icon"><BiBarChartAlt /></div>
-              <h3 className="">{t("sectionOne.one-digital")}</h3>
+              <div className="one-icon"><BiBarChartAlt /></div>
+              <h3 className="one-teetle">{t("sectionOne.one-digital")}</h3>
               <p className="one-text">{t("sectionOne.one-text")}</p>
               <button className="one-btn">{t("sectionOne.one-btn")}</button>
             </li>
