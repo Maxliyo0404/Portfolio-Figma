6d2cd4d90cb3c71008aca3e6dbcce5723e6f5d91
Mahliyo <gasandigital13@gmail.com>
2026-05-21 12:29:01 +0500

    sectionOne wrapper i18n

--- src/Components/Main/SectionOne/SectionOne.jsx
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -1,9 +1,25 @@
+import { useTranslation } from "react-i18next";
 import "./SectionOne.css";
 import React from 'react'
 
 function SectionOne() {
+  const [t, i18n] = useTranslation();
   return (
-    <div>SectionOne</div>
+    <div className="sectionOne">
+      <div className="container">
+        <div className="sectionOne-wrapper">
+          <h2 className="one-title"></h2>
+          <ul className="one-list">
+            <li className="one-item">
+              <div className="icon"></div>
+              <h3 className=""></h3>
+              <p className="one-btn"></p>
+              <button className="one-btn"></button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
