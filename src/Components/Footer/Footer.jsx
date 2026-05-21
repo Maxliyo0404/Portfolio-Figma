fe529c145f5a2464c51669ba251e4cba002e8d86
Mahliyo <gasandigital13@gmail.com>
2026-05-21 17:15:55 +0500

    footer startclear

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./Footer.css";
 import React from 'react'
+import { FaArrowUp } from "react-icons/fa";
 
 function Footer() {
     const [t, i18n] = useTranslation();
@@ -9,9 +10,7 @@ function Footer() {
         <div className="container">
             <div className="footer-wrapper">
                 <a className="footer-link" href="#">{t("footer.footer-link")}</a>
-                <div className="footer-icon">
-                    
-                </div>
+                <div className="footer-icon"><FaArrowUp /></div>
             </div>
         </div>
     </div>
