40e50f093219a151b4c6ff55c669a850663c5f3e
Mahliyo <gasandigital13@gmail.com>
2026-05-21 12:32:23 +0500

    sectionOne wrapper i18n

--- src/Components/Main/SectionOne/SectionOne.jsx
+++ src/Components/Main/SectionOne/SectionOne.jsx
@@ -8,13 +8,13 @@ function SectionOne() {
     <div className="sectionOne">
       <div className="container">
         <div className="sectionOne-wrapper">
-          <h2 className="one-title"></h2>
+          <h2 className="one-title">{t("sectionOne.one-title")}</h2>
           <ul className="one-list">
             <li className="one-item">
               <div className="icon"></div>
-              <h3 className=""></h3>
-              <p className="one-btn"></p>
-              <button className="one-btn"></button>
+              <h3 className="">{t("sectionOne.one-btn")}</h3>
+              <p className="one-text">{t("sectionOne.one-text")}</p>
+              <button className="one-btn">{t("sectionOne.one-btn")}</button>
             </li>
           </ul>
         </div>
